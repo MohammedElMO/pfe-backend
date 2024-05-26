23ebebd141d363e3983791ba95b0a3700b78c2ce
MohammedElMO <elamranimohammed104@gmail.com>
2024-05-26 13:04:30 +0100

    cleaning

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ import authRouter from "./controllers/auth"
 import userRouter from "./controllers/user"
 import { json } from "express"
 import cors from "cors"
-import cookieParser from "cookie-parser"
+// import cookieParser from "cookie-parser"
 import ProfileRouter from "./controllers/profile"
 import FavouritesRouter from "./controllers/favourites"
 import PharmacyCity from "./controllers/city"
@@ -11,12 +11,12 @@ const app = express()
 
 app.use(
   cors({
-    origin: "*",
+    origin: "https://pfe-khaliha-3la-lah.vercel.app/",
   }),
 )
 // "http://localhost:5173"
 app.use(json())
-app.use(cookieParser())
+// app.use(cookieParser())
 
 app.use("/api", authRouter, ProfileRouter, FavouritesRouter, PharmacyCity)
 app.use("/api/user", userRouter)
--- utils/picker.ts
+++
@@ -1,5 +0,0 @@
-export const  pick = (arr: number[], index: number) => {
-  if (index < arr.length && index >= 0) return arr[index];
-
-  return 0;
-}
